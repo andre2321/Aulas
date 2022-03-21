b95b180985ed7059b4c325ae734c3511764aeb42
andre2321 <78964428+andre2321@users.noreply.github.com>
2022-03-21 17:54:56 -0300

    Update codigo.ino

--- Exercicios/ex3/codigo.ino
+++ Exercicios/ex3/codigo.ino
@@ -10,19 +10,28 @@ void loop()
 {
   digitalWrite(13, HIGH);
   delay(2000);
+  
   digitalWrite(5, HIGH);
   delay(2000);
+  
  digitalWrite(4, HIGH);
   delay(2000);
+  
   digitalWrite(8, HIGH);
-   delay(2000); 
+   delay(2000);
+  
   digitalWrite(12, HIGH);
  delay(2000);
   
   digitalWrite(13, LOW);
+  
   digitalWrite(5, LOW);
+  
   digitalWrite(4, LOW);
+  
   digitalWrite(8, LOW);
+  
   digitalWrite(12, LOW);
+  
   delay(1000);
 }
