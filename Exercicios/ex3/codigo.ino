b68a7c23ac50bad88d0070139aa6281669f5f31a
andre2321 <78964428+andre2321@users.noreply.github.com>
2022-03-21 16:38:40 -0300

    Update codigo.ino

--- Exercicios/ex3/codigo.ino
+++ Exercicios/ex3/codigo.ino
@@ -1,1 +1,28 @@
-
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(5, HIGH);
+  delay(2000);
+ digitalWrite(4, HIGH);
+  delay(2000);
+  digitalWrite(8, HIGH);
+   delay(2000); 
+  digitalWrite(12, HIGH);
+ delay(2000);
+  
+  digitalWrite(13, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(12, LOW);
+  delay(1000);
+}
