3a14ff752da7b8ea6e6b75906d20fd7808d46790
andre2321 <78964428+andre2321@users.noreply.github.com>
2022-03-21 16:03:50 -0300

    Update codigo.ino

--- Exercicios/ex2/codigo.ino
+++ Exercicios/ex2/codigo.ino
@@ -1,15 +1,1 @@
-void setup()
-{
-  pinMode(13, OUTPUT);
-  pinMode(5, OUTPUT); 
-}
 
-void loop()
-{ 
-  digitalWrite(13, HIGH);
-  delay(10000); 
-  digitalWrite(13, LOW);
-  digitalWrite(5, HIGH);
-  delay(10000); 
-   digitalWrite(5, LOW);   
-}
