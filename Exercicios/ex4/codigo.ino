e2e39db1e2acc9b45ad5f105bd32ec97d64e50be
andre2321 <78964428+andre2321@users.noreply.github.com>
2022-03-21 16:44:58 -0300

    Update codigo.ino

--- Exercicios/ex4/codigo.ino
+++ Exercicios/ex4/codigo.ino
@@ -1,1 +1,25 @@
+int button = 2;
+int led1 = 13;
+int lastButtonState = 0;
+int buttonState = 0;
+int count = 0;
 
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(button, INPUT);
+}
+void loop() {
+  buttonState = digitalRead(button);
+    if (buttonState != lastButtonState) {
+        if (buttonState == HIGH) {
+        count++;
+        if (count % 4 != 0) {
+          digitalWrite(led1, HIGH);
+        } else {
+          digitalWrite(led1, LOW);
+        }
+        delay(5);
+        }
+    }
+      lastButtonState = buttonState;
+}
