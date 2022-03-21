b31480376538f40bb833a1e993b706b97557de3a
andre2321 <78964428+andre2321@users.noreply.github.com>
2022-03-21 17:51:44 -0300

    Update codigo.ino

--- Exercicios/ex5/codigo.ino
+++ Exercicios/ex5/codigo.ino
@@ -1,1 +1,46 @@
+int button = 2;
+int led1 = 13;
+int lastButtonState = 0;
+int estadoButton = 0;
 
+
+void setup()
+{
+  pinMode(led1 , OUTPUT);
+  pinMode (button , INPUT);
+  
+}
+
+void loop()
+{
+  estadoButton = digitalRead (button);
+  if ( estadoButton == HIGH ) { 
+    digitalWrite(led1 , LOW);
+    delay(1000);
+  }
+  else { 
+    digitalWrite (led1 , HIGH );  
+  }int button = 2;
+int led1 = 13;
+int lastButtonState = 0;
+int estadoButton = 0;
+
+
+void setup()
+{
+  pinMode(led1 , OUTPUT);
+  pinMode (button , INPUT);
+  
+}
+
+void loop()
+{
+  estadoButton = digitalRead (button);
+  if ( estadoButton == HIGH ) { 
+    digitalWrite(led1 , LOW);
+    delay(1000);
+  }
+  else { 
+    digitalWrite (led1 , HIGH );  
+  }
+}
